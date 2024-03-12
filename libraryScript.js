a82358076171e57246e40f20521441ebe20516a2
keylimegreen <doctorjs2024@gmail.com>
2024-03-12 14:49:42 -0400

    library work add button

--- libraryScript.js
+++ libraryScript.js
@@ -1,11 +1,28 @@
-event.preventDefault()
+
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author) {
+  this.title = title
+  this.author = author
+  this.read = false
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+function displayBooks() {
+
+}
+
+window.addEventListener("load", (event) => {
+  const book1 = new Book('Huckleberry Fin', 'Mark Twain')
+  myLibrary.push(book1)
+  displayBooks()
+})
+
+const button = document.getElementById('add-book')
+button.addEventListener("click", ()=> {
+  dialog.showModal();
+})
